92939a27f2e0f5aa5271ce6b85e976f1ada83a00
James Chryssanthacopoulos <jchryssanthacopoulos@gmail.com>
2015-05-28 17:25:03 -0500

    Extended aggregate.sh to aggregate areas

--- bin/aggregate/aggregate.sh
+++ bin/aggregate/aggregate.sh
@@ -18,9 +18,12 @@ for c in maize soybean sorghum cotton wheat; do
    rm crop_progress.nc4
 
    # aggregate yield reference
-   ydaggfile=$indir/$c/final/$c.reference.agg.nc4
+   aggfile=$indir/$c/final/$c.reference.agg.nc4
+   aggareafile=$indir/$c/final/$c.reference.agg.area.nc4
    ncks -h -d irr,0,1 $indir/$c/final/$c.reference.nc4 reference.nc4
    ncatted -O -h -a long_name,irr,m,c,"ir, rf" reference.nc4 reference.nc4
-   /project/joshuaelliott/psims/bin/agg.out.py -i reference.nc4:yield -w weights.nc4 -a $aggfile -n 10 -l time -o $ydaggfile
-   rm weights.nc4 reference.nc4
+   /project/joshuaelliott/psims/bin/agg.out.py -i reference.nc4:yield -w weights.nc4 -a $aggfile -n 10 -l time -o $aggfile # yield
+   /project/joshuaelliott/psims/bin/agg.out.py -i reference.nc4:area,area_mirca -w weights.nc4 -t sum -a $aggfile -n 10 -l time -o $aggareafile # area
+   ncks -h -A $aggareafile $aggfile
+   rm weights.nc4 reference.nc4 $aggareafile
 done
